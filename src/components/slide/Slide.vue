bbe50abff29cb0e8bb93e18ea2daf84df6a10b13
ToMattBan <souaquelecleyton@yandex.com>
2022-08-01 18:55:20 -0300

    Adicionado Scoped no CSS

--- src/components/slide/Slide.vue
+++ src/components/slide/Slide.vue
@@ -14,7 +14,7 @@
       </div>
     </div>
     <div class="image-container">
-      <img alt="" class="image slide-img1" :src="image"/>
+      <img alt="" class="image slide-img1" :src="image" />
     </div>
   </div>
 </template>
@@ -33,7 +33,7 @@ export default defineComponent({
   },
   methods: {
     btnClick(action) {
-      switch(action) {
+      switch (action) {
         case 'openRockstar':
           this.openLauncher('rockstar');
           break;
@@ -61,4 +61,172 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.slide {
+  display: none;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+
+  &.is-active {
+    display: block;
+  }
+
+  &.is-loaded {
+    opacity: 1;
+  }
+
+  .caption {
+    margin-top: 250px;
+    padding: 0 100px;
+    width: 1000px;
+    margin-left: 245px;
+  }
+
+  .image-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    object-fit: cover;
+    height: 650px;
+    background-position: 50% 30%;
+    background-position: center;
+    z-index: 1;
+    background-size: cover;
+
+    &::before {
+      content: "";
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.5);
+    }
+  }
+
+  .image {
+    width: 101%;
+    background-position: 50% 30%;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    object-fit: cover;
+    height: 100%;
+  }
+
+  .title {
+    margin: 0 auto 5px;
+    max-width: 1000px;
+    font: 300 50px/1.2 "Oxygen", sans-serif;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    font-weight: 700;
+    text-shadow: 5px 1px 10px rgba(0, 0, 0, 0.8);
+  }
+
+  .slideshow .slide .text {
+    margin: 0 auto;
+    max-width: 1000px;
+    font-size: 20px;
+    line-height: 1.4;
+  }
+
+  .text p {
+    margin: 0;
+    text-shadow: 1px 2px 5px rgba(0, 0, 0, 0.8);
+  }
+
+  .slide-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    color: #fff;
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .btn {
+       margin: 15px 0 0;
+    border-color: #707070;
+    margin-left: 0px;
+      display: inline-block;
+      padding: 15px 20px 13px 20px;
+      color: #fff;
+      font-weight: 800;
+      text-decoration: none;
+      position: relative;
+      background-color: rgb(4 4 4 / 32%);
+      -webkit-backdrop-filter: blur(10px);
+      backdrop-filter: blur(10px);
+      border: 1px solid #e1e1e1;
+      border-radius: 2em;
+      font: 14px/1.2 "Oxygen", sans-serif;
+      letter-spacing: 0.2em;
+      text-align: center;
+      text-indent: 2px;
+      text-transform: uppercase;
+      transition: color 0.1s linear 0.05s;
+      cursor: pointer;
+      box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+        0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 100%;
+        height: 1px;
+        background: #e1e1e1;
+        border-radius: 2em;
+        z-index: 1;
+        opacity: 0;
+        transition: height 0.2s ease, top 0.2s ease, opacity 0s linear 0.2s;
+      }
+
+      &::after {
+        transition: border 0.1s linear 0.05s;
+      }
+
+      &:hover {
+        color: #373737;
+        transition: color 0.1s linear 0s;
+
+        &::before {
+          top: 0;
+          height: 100%;
+          opacity: 1;
+          transition: height 0.2s ease, top 0.2s ease, opacity 0s linear 0s;
+        }
+
+        &::after {
+          border-color: #373737;
+          transition: border 0.1s linear 0s;
+        }
+      }
+
+      .btn-inner {
+        font-weight: 600;
+        position: relative;
+        z-index: 2;
+      }
+    }
+
+  }
+}
+</style>
--- src/components/slide/SlideHome.vue
+++ src/components/slide/SlideHome.vue
@@ -3,23 +3,14 @@
     <section class="slideshow">
       <div class="slideshow-inner">
         <div class="slides">
-          <Slide
-            v-for="(slide, index) in slides"
-            :isActive="index == 0"
-            :image="slide.image"
-            :name="slide.name"
-            :desc="slide.texts[userLanguage].descText"
-            :btnText="slide.texts[userLanguage].buttonText"
-            :btnAction="slide.btnAction"
-          />
+          <Slide v-for="(slide, index) in slides" :isActive="index == 0" :image="slide.image" :name="slide.name"
+            :desc="slide.texts[userLanguage].descText" :btnText="slide.texts[userLanguage].buttonText"
+            :btnAction="slide.btnAction" />
 
           <div class="pagination">
             <div class="pagination2">
-              <div
-                v-for="(slide, index) in slides"
-                :class="`item ${index == 0 ? 'is-active' : ''}`"
-                @click="changeSlide(index)"
-              >
+              <div v-for="(slide, index) in slides" :class="`item ${index == 0 ? 'is-active' : ''}`"
+                @click="changeSlide(index)">
                 <span class="icon">{{ index + 1 }}</span>
               </div>
             </div>
@@ -203,68 +194,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.btn {
-  display: inline-block;
-  padding: 15px 20px 13px 20px;
-  color: #fff;
-  font-weight: 800;
-  text-decoration: none;
-  position: relative;
-  background-color: rgb(4 4 4 / 32%);
-  -webkit-backdrop-filter: blur(10px);
-  backdrop-filter: blur(10px);
-  border: 1px solid #e1e1e1;
-  border-radius: 2em;
-  font: 14px/1.2 "Oxygen", sans-serif;
-  letter-spacing: 0.2em;
-  text-align: center;
-  text-indent: 2px;
-  text-transform: uppercase;
-  transition: color 0.1s linear 0.05s;
-  cursor: pointer;
-  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
-    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
-
-  &::before {
-    content: "";
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 0;
-    width: 100%;
-    height: 1px;
-    background: #e1e1e1;
-    border-radius: 2em;
-    z-index: 1;
-    opacity: 0;
-    transition: height 0.2s ease, top 0.2s ease, opacity 0s linear 0.2s;
-  }
-  &::after {
-    transition: border 0.1s linear 0.05s;
-  }
-  &:hover {
-    color: #373737;
-    transition: color 0.1s linear 0s;
-
-    &::before {
-      top: 0;
-      height: 100%;
-      opacity: 1;
-      transition: height 0.2s ease, top 0.2s ease, opacity 0s linear 0s;
-    }
-    &::after {
-      border-color: #373737;
-      transition: border 0.1s linear 0s;
-    }
-  }
-  .btn-inner {
-    font-weight: 600;
-    position: relative;
-    z-index: 2;
-  }
-}
-
+<style lang="scss" scoped>
 .slideshow {
   overflow: hidden;
   position: relative;
@@ -289,118 +219,10 @@ export default {
     z-index: 1;
   }
 
-  .slide {
-    display: none;
-    overflow: hidden;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    opacity: 0;
-    transition: opacity 0.3s ease;
-
-    &.is-active {
-      display: block;
-    }
-
-    &.is-loaded {
-      opacity: 1;
-    }
-
-    .caption {
-      margin-top: 250px;
-      padding: 0 100px;
-      width: 1000px;
-      margin-left: 245px;
-    }
-
-    .image-container {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      object-fit: cover;
-      height: 650px;
-      background-position: 50% 30%;
-      background-position: center;
-      z-index: 1;
-      background-size: cover;
-
-      &::before {
-        content: "";
-        display: block;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0, 0, 0, 0.5);
-      }
-    }
-
-    .image {
-      width: 101%;
-      background-position: 50% 30%;
-      background-size: 100%;
-      background-repeat: no-repeat;
-      object-fit: cover;
-      height: 100%;
-    }
-
-    .title {
-      margin: 0 auto 5px;
-      max-width: 1000px;
-      font: 300 50px/1.2 "Oxygen", sans-serif;
-      letter-spacing: 0.2em;
-      text-transform: uppercase;
-      font-weight: 700;
-      text-shadow: 5px 1px 10px rgba(0, 0, 0, 0.8);
-    }
-
-    .slideshow .slide .text {
-      margin: 0 auto;
-      max-width: 1000px;
-      font-size: 20px;
-      line-height: 1.4;
-    }
-
-    .text p {
-      margin: 0;
-      text-shadow: 1px 2px 5px rgba(0, 0, 0, 0.8);
-    }
-
-    .btn {
-      margin: 15px 0 0;
-      border-color: #707070;
-      margin-left: 0px;
-
-      &::before {
-        background: #fff;
-      }
-    }
-  }
-
-  .slide-content {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 2;
-    color: #fff;
-    font-family: "Roboto", sans-serif;
-    font-weight: 500;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 
   .pagination {
     position: absolute;
-    bottom: 35px;
+    bottom: 12px;
     width: 100%;
     right: 0;
     height: 12px;
@@ -440,7 +262,7 @@ export default {
         transition: width 0.2s ease;
       }
 
-      & + .page {
+      &+.page {
         margin-left: -2px;
       }
 
@@ -449,42 +271,6 @@ export default {
         background-color: #fff;
       }
     }
-  }
-
-  .arrows {
-    .arrow {
-      margin: -33px 0 0;
-      padding: 20px;
-      position: absolute;
-      top: 50%;
-      cursor: pointer;
-      z-index: 3;
-    }
-
-    .prev {
-      left: 275px;
-
-      &:hover .svg {
-        left: -10px;
-      }
-    }
-
-    .next {
-      right: 30px;
-
-      &:hover .svg {
-        left: 10px;
-      }
-    }
-  }
-
-  .svg {
-    position: relative;
-    left: 0;
-    width: 14px;
-    height: 26px;
-    fill: #fff;
-    transition: left 0.2s ease;
   }
 
   .pagination2 {
